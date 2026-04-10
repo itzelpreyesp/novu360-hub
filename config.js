134107258b570690ed469dc3bbfa44ea8ec5dd97
itzelpreyesp <itzelp.reyesp@gmail.com>
2026-04-10 16:26:21 -0600

    cambios en prospector ia v2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://tmyniibsccxhfifhcqpa.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_jY3aG232zXsij1o2zDAWig_bbhfLIjO';
-const GEMINI_KEY = '[AIzaSyCmkUoSEGxz9UafGtTfd0idhXSnpabL9_M]';
+const GEMINI_KEY = 'AIzaSyCmkUoSEGxz9UafGtTfd0idhXSnpabL9_M';
 window.CONFIG = { SUPABASE_URL, SUPABASE_ANON_KEY, GEMINI_KEY };
 
 // Variable global para el cliente de Supabase
