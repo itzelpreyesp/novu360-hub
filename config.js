74015932c6a2c57f09926e871c55733845fcb90e
itzelpreyesp <itzelp.reyesp@gmail.com>
2026-04-10 16:52:44 -0600

    prospecto v5

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://tmyniibsccxhfifhcqpa.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_jY3aG232zXsij1o2zDAWig_bbhfLIjO';
-const GEMINI_KEY = 'AIzaSyCmkUoSEGxz9UafGtTfd0idhXSnpabL9_M';
+const GEMINI_KEY = 'AIzaSyBYBeU37nvcDlwpzftvt2HoYGyHh2RJUSQ';
 window.CONFIG = { SUPABASE_URL, SUPABASE_ANON_KEY, GEMINI_KEY };
 
 // Exponer variables globales sueltas para compatibilidad
